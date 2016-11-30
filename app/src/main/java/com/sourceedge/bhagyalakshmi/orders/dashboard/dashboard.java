6ebed28a4e1c9cb69ed9f5d79d10dae80234cc91
rahul <rahulupadhyav@gmail.com>
2016-11-30 18:03:42 +0530

    dashboard drawer ui done

--- app/src/main/java/com/sourceedge/bhagyalakshmi/orders/dashboard/dashboard.java
+++ app/src/main/java/com/sourceedge/bhagyalakshmi/orders/dashboard/dashboard.java
@@ -3,6 +3,8 @@ package com.sourceedge.bhagyalakshmi.orders.dashboard;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -10,12 +12,35 @@ import android.view.View;
 import com.sourceedge.bhagyalakshmi.orders.R;
 
 public class dashboard extends AppCompatActivity {
+    Toolbar toolbar;
+    DrawerLayout drawer;
+    ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("BAGHYALAKSHMI TRADERS");
         setSupportActionBar(toolbar);
+        drawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
+        setupDrawer();
+    }
+
+    private void setupDrawer() {
+        // Drawer object Assigned to the view
+        mDrawerToggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.opendrawer, R.string.closedrawer) {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+            }
+        };
+        drawer.setDrawerListener(mDrawerToggle); // Drawer Listener set to the Drawer toggle
+        mDrawerToggle.syncState();               // Finally we set the drawer toggle sync State
     }
 }
