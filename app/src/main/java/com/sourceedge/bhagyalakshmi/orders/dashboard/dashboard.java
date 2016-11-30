c2a81815ee2d985e3733059cb1cfc7a4f6589600
rahul <rahulupadhyav@gmail.com>
2016-11-30 16:24:37 +0530

    login ui

--- app/src/main/java/com/sourceedge/bhagyalakshmi/orders/dashboard/dashboard.java
+++ app/src/main/java/com/sourceedge/bhagyalakshmi/orders/dashboard/dashboard.java
@@ -1,19 +1,21 @@
-package com.sourceedge.bhagyalakshmi.orders.login;
+package com.sourceedge.bhagyalakshmi.orders.dashboard;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.sourceedge.bhagyalakshmi.orders.R;
 
-public class Login extends AppCompatActivity {
+public class dashboard extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_dashboard);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
-
 }
