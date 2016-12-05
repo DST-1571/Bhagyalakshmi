74be770e4c6c2a0308948cb00f6d01fd0f7074d6
vijayalakshmi <asvij.04@gmail.com>
2016-12-05 12:31:23 +0530

    test

--- app/src/main/java/com/sourceedge/bhagyalakshmi/orders/dashboard/controller/Dashboard.java
+++ app/src/main/java/com/sourceedge/bhagyalakshmi/orders/dashboard/controller/Dashboard.java
@@ -16,6 +16,8 @@ public class Dashboard extends AppCompatActivity {
     DrawerLayout drawer;
     ActionBarDrawerToggle mDrawerToggle;
     RecyclerView mySalesRecycler;
+
+    //test vcs
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
