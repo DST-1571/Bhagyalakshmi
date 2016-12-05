13f3f76847d02dbc0011a55684613c70a7adfcd2
vijayalakshmi <asvij.04@gmail.com>
2016-12-05 17:22:25 +0530

    test

--- app/src/main/java/com/sourceedge/bhagyalakshmi/orders/salesperson/controller/SalesPerson.java
+++ app/src/main/java/com/sourceedge/bhagyalakshmi/orders/salesperson/controller/SalesPerson.java
@@ -1,28 +1,31 @@
 package com.sourceedge.bhagyalakshmi.orders.salesperson.controller;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.sourceedge.bhagyalakshmi.orders.R;
 import com.sourceedge.bhagyalakshmi.orders.support.Class_Genric;
 
-public class SalesPersonLookUp extends AppCompatActivity {
+public class SalesPerson extends AppCompatActivity {
     Toolbar toolbar;
     DrawerLayout drawer;
     ActionBarDrawerToggle mDrawerToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sales_person_lookup);
-        Class_Genric.setOrientation(SalesPersonLookUp.this);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        toolbar.setTitle("BAGHYALAKSHMI TRADERS");
+        setContentView(R.layout.activity_sales_person);
+         toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Class_Genric.setOrientation(SalesPerson.this);
         setSupportActionBar(toolbar);
+        toolbar.setTitle("BAGHYALAKSHMI TRADERS");
         drawer = (DrawerLayout) findViewById(R.id.navigation_drawer);
-        Class_Genric.setupDrawer(toolbar,drawer,mDrawerToggle,SalesPersonLookUp.this);
+        Class_Genric.setupDrawer(toolbar,drawer,mDrawerToggle,SalesPerson.this);
     }
 
 }
