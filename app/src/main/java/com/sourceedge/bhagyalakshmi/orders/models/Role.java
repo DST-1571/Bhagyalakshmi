befb1d35d0e051ee47cccf4fa641a3a1e9712e47
rahul <rahulupadhyav@gmail.com>
2016-12-08 11:45:22 +0530

    role changed

--- app/src/main/java/com/sourceedge/bhagyalakshmi/orders/models/Role.java
+++ app/src/main/java/com/sourceedge/bhagyalakshmi/orders/models/Role.java
@@ -1,28 +1,16 @@
 package com.sourceedge.bhagyalakshmi.orders.models;
 
-<<<<<<< Updated upstream
 import java.sql.Timestamp;
-
-/**
- * Created by Deekshith on 06-12-2016.
-=======
-/**
- * Created by Centura User1 on 06-12-2016.
->>>>>>> Stashed changes
- */
 
 public class Role {
     private String Id;
     private String Name;
 
-<<<<<<< Updated upstream
-=======
     public Role(){
         Id="";
         Name="";
     }
 
->>>>>>> Stashed changes
     public String getId() {
         return Id;
     }
@@ -38,15 +26,5 @@ public class Role {
     public void setName(String name) {
         Name = name;
     }
-<<<<<<< Updated upstream
 
-    public Role()
-    {
-        Id="";
-        Name="";
-
-    }
-
-=======
->>>>>>> Stashed changes
 }
